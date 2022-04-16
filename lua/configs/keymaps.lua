c96dec49ee8553dce6740fd0d6b696c5d145a60b
c-my <cmy1113@outlook.com>
2022-04-16 10:54:35 +0800

    keymap: use 0.7 new api

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,9 +1,8 @@
 local opts = {
-    noremap = true,
     silent = true
 }
 
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', opts)
+vim.keymap.set('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -11,33 +10,33 @@ vim.g.maplocalleader = ' '
 -- == re-indents the line to suit its new position
 -- gc reselects the last visual block
 -- = re-indents the block
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', opts)
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', opts)
-vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', opts)
-vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', opts)
-vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
-vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', opts)
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', opts)
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', opts)
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', opts)
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
 
 -- Navigating window
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', opts)
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', opts)
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', opts)
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 
 -- Keymaps for NvimTree
-vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeFindFileToggle <CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>a', ':NvimTreeFocus <CR>', opts)
+vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle <CR>', opts)
+vim.keymap.set('n', '<leader>a', ':NvimTreeFocus <CR>', opts)
 
 -- Keymaps for Telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files <CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep <CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers <CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags <CR>', opts)
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files <CR>', opts)
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep <CR>', opts)
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers <CR>', opts)
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags <CR>', opts)
 
 -- Keymaps for LSP
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+vim.keymap.set('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.keymap.set('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
