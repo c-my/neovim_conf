efc949b0ff65625a4683ccea7778f2bbabedb2a6
c-my <cmy1113@outlook.com>
2022-04-28 23:47:04 +0800

    lsp: update new lsp_installer config

--- lua/configs/plugins/lsp_installer.lua
+++ lua/configs/plugins/lsp_installer.lua
@@ -1,5 +1,19 @@
 local lsp_installer = require('nvim-lsp-installer')
 
+lsp_installer.settings({
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
+lsp_installer.setup {}
+
+
+local lspconfig = require('lspconfig')
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -29,39 +43,41 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+lspconfig.sumneko_lua.setup({
+    on_attach = on_attach,
+    settings = require('configs.plugins.lsp_settings.sumneko_lua'),
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+
+})
+
+lspconfig.jdtls.setup({
+    on_attach = on_attach,
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+})
+
 -- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
 -- or if the server is already installed).
-lsp_installer.on_server_ready(function(server)
-    local opts = {
-        on_attach = on_attach,
-        flags = {
-            -- This will be the default in neovim 0.7+
-            debounce_text_changes = 150
-        },
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    }
-
-    if server.name == 'sumneko_lua' then
-        local sumneko_lua_opts = require('configs.plugins.lsp_settings.sumneko_lua')
-        opts.settings = sumneko_lua_opts
-    end
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
-
-lsp_installer.settings({
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
-    }
-})
+-- lsp_installer.on_server_ready(function(server)
+--     local opts = {
+--         on_attach = on_attach,
+--         flags = {
+--             -- This will be the default in neovim 0.7+
+--             debounce_text_changes = 150
+--         },
+--         capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+--     }
+--
+--     if server.name == 'sumneko_lua' then
+--         local sumneko_lua_opts = require('configs.plugins.lsp_settings.sumneko_lua')
+--         opts.settings = sumneko_lua_opts
+--     end
+--     -- (optional) Customize the options passed to the server
+--     -- if server.name == "tsserver" then
+--     --     opts.root_dir = function() ... end
+--     -- end
+--
+--     -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+--     -- before passing it onwards to lspconfig.
+--     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+--     server:setup(opts)
+-- end)
