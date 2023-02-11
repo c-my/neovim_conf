b7b98a0985a44552bb9ba35b6ac40199d6ac3bf3
c-my <cmy1113@outlook.com>
2023-02-11 23:06:19 +0800

    add some autocmds

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require 'configs.options'
-require 'configs.keymaps'
-require 'configs.lazy'
+require("configs.options")
+require("configs.keymaps")
+require("configs.autocmds")
+require("configs.lazy")
---
+++ lua/configs/autocmds.lua
@@ -0,0 +1,19 @@
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+    pattern = {
+        "qf", -- quickfix window
+        "help",
+        "man",
+        "notify",
+        "lspinfo",
+        "spectre_panel",
+        "startuptime",
+        "tsplayground",
+        "PlenaryTestPopup",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<Cmd>close<CR>", { buffer = event.buf, silent = true, desc = "Close Buffer" })
+    end,
+})
