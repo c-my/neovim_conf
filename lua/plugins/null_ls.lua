7fc9b3b9e59805a7d7989e521bc64b85b23aafd0
c-my <cmy1113@outlook.com>
2023-02-13 19:00:02 +0800

    null-ls: fix setup

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -2,7 +2,9 @@ return { { "jose-elias-alvarez/null-ls.nvim",
     opts = function()
         local null_ls = require("null-ls")
         return {
-            -- null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.black,
+            sources = {
+                -- null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black,
+            }
         }
     end } }
