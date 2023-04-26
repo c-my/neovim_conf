4d3e820499e3d4d501363f35098d231f443a9134
c-my <cmy1113@outlook.com>
2023-04-26 20:26:16 +0800

    reformat

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,11 +1,15 @@
-return { { "jose-elias-alvarez/null-ls.nvim",
-    lazy = true,
-    opts = function()
-        local null_ls = require("null-ls")
-        return {
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.black,
+return {
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        lazy = true,
+        opts = function()
+            local null_ls = require("null-ls")
+            return {
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black,
+                },
             }
-        }
-    end } }
+        end,
+    },
+}
